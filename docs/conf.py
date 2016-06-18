3d58e359e5c2ad09b13807a0efd20cb6bef1b150
dauledk <alex@wukwuk.dk>
2016-06-18 15:03:12 +0200

    removed napoleon dependencie

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinxcontrib.napoleon',
+              #'sphinxcontrib.napoleon',
               'sphinx.ext.mathjax',
               'sphinx.ext.extlinks']
 
